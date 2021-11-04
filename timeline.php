a920edf3c61fbe24b8a5929d9e37f6d5d3839136
Raharja214 <72438824+Raharja214@users.noreply.github.com>
2021-11-04 14:43:21 +0700

    tampilan utama web

---
+++ timeline.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RDPWEB Halaman Utama</title>
+</head>
+<body>
+    <div class="col-md-6">
+        <img class="img img-responsive" src="img/rdpweb.png" />
+    </div>
+    <button onclick="document.location='tabel.php'">Tabel</button>
+    <button onclick="document.location='chart.php'">Chart</button>
+    <button onclick="document.location='index.php'">Keluar</button>
+    <h2>Selamat Datang di RDPWEB! Silahkan pilih menu yang tertera di atas.</h2>
+</body>
+</html>
